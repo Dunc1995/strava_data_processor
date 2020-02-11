b24ccd8961e2c140290474d65b82d01fe9b13e59
Duncan Bailey <DPBailey1995@outlook.com>
2020-02-11 22:00:51 +0000

    changes

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Decrypt the secret file
+# Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
