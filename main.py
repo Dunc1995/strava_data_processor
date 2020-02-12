29d5a1849fa91b572e5738b78dd5d8624444c8c9
Duncan Bailey <DPBailey1995@outlook.com>
2020-02-12 09:00:30 +0000

    edits

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 
 #region #*----------Modules----------
 import os
+import os.path
 import json
 import strava_api
 from pygdrive3 import service
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import pickle
 #endregion
+
+HOME = os.environ.get('HOME')
 
 #region #*----------Main Function----------
 #? Main application function.
@@ -16,8 +23,20 @@ def main():
     # result = strava_requests.get_activity('3079381521')
     # splits = strava_api.fetch_activity_splits(result)
 
-    drive_service = service.DriveService(os.environ.get('GOOGLE_DRIVE_SECRET') )
-    drive_service.auth()
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('{}/secrets/token.pickle'.format(HOME)):
+        with open('{}/secrets/token.pickle'.format(HOME), 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+    drive_service = service.build('drive', 'v3', credentials=creds)
 
     folder = drive_service.create_folder('Strava_Data_App')
     file = drive_service.upload_file('Testing.json', './hello_world.json', folder)
