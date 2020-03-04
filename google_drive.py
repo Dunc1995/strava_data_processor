fef42c85ddbb72efd666408add432434910f16c1
Duncan <dpbailey1995@outlook.com>
2020-03-04 14:19:28 +0000

    test build

--- google_drive.py
+++ google_drive.py
@@ -40,7 +40,7 @@ class client():
                                                 fields='nextPageToken, files(id, name)',
                                                 pageToken=page_token).execute()
             for file in response.get('files', []):
-                # Process change
+                # Process chang
                 print('Found file: %s (%s)' % (file.get('name'), file.get('id')))
             page_token = response.get('nextPageToken', None)
             if page_token is None:
